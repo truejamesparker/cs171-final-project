0cbfeb1db47f81fdde466071c7eec01689e03a0f
David Hacker <davidhacker@dhcp-10-250-94-113.harvard.edu>
2021-11-22 11:46:27 -0500

    notable homes

--- js/famousVis.js
+++ js/famousVis.js
@@ -42,6 +42,7 @@ class FamousVis {
 		vis.data.forEach(d => {
 			d.completed = yearParser(d.completed)
 			d.sqft = +d.sqft
+			d.rank = +d.rank
 		})
 
 		console.log(vis.data)
@@ -50,6 +51,14 @@ class FamousVis {
 			.domain(d3.extent(vis.data, d => {return d.completed}))
 			.range([0, vis.width])
 
+		vis.xAxisGroup = vis.svg.append("g")
+			.attr("class", "x-axis axis");
+
+
+
+		vis.tooltip = d3.select("body").append('div')
+			.attr('class', "tooltip")
+			.attr('id', 'famousTooltip')
 
 		vis.s = d3.scaleLinear()
 			.range([0, Math.sqrt(5000)])
@@ -66,8 +75,7 @@ class FamousVis {
 		let vis = this;
 
 		// TODO
-
-		vis.displayData = vis.data;
+		vis.displayData = vis.data
 
 		// Update the visualization
 		vis.updateVis();
@@ -89,6 +97,43 @@ class FamousVis {
 			.attr("y", (d,i) => 50)
 			.attr("width", d => vis.s(d.sqft))
 			.attr("height", d => vis.s(d.sqft))
+			.on('mouseover', function(event, d){
+				d3.select(this)
+					.attr('stroke-width', '2px')
+					.attr('stroke', 'black')
+					.attr('fill', 'rgba(173,222,255,0.62)');
+				vis.tooltip
+					.style("opacity", 1)
+					.style("left", event.pageX + 20 + "px")
+					.style("top", event.pageY + "px")
+					.html(`
+         <div style="border: thin solid grey; border-radius: 5px; background: white; padding: 20px">
+             <a href="https://en.wikipedia.org/wiki/${d.name.split(' ').join('_')}">${d.name}</a>      
+             <h6>${d.sqft} square feet</h6>                    
+         </div>`);
+			})
+			.on('mouseout', function(event, d){
+				d3.select(this)
+					.attr('stroke-width', '0px')
+					.attr("fill", "#B22234")
+
+				vis.tooltip
+					.style("opacity", 0)
+					.style("left", 0)
+					.style("top", 0)
+					.html(``);
+			});
+
+		vis.xAxis = d3.axisBottom()
+			.scale(vis.x)
+		vis.yAxis = d3.axisLeft()
+			.scale(vis.y)
+
+		vis.svg.select(".x-axis")
+			.attr("transform", "translate(" + vis.margin.left + ",118)")
+			.style("stroke-width", 2)
+			.transition()
+			.call(vis.xAxis);
 
 
 	}
