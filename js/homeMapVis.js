29c3b93fe0c65a7664a4f95225e553823570ec4a
James Parker <parkerpatriot@gmail.com>
2021-12-04 17:00:55 -0500

    Adding linked area vis to map

--- js/homeMapVis.js
+++ js/homeMapVis.js
@@ -28,10 +28,10 @@ class HomeMapVis {
 
 		// TODO
 
-		vis.margin = { top: 40, right: 20, bottom: 40, left: 20 };
+		vis.margin = { top: 40, right: 30, bottom: 40, left: 30 };
 
 		vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-		vis.height = 500 - vis.margin.top - vis.margin.bottom; //document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.top - vis.margin.bottom;
+		vis.height = 300 - vis.margin.top - vis.margin.bottom; //document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.top - vis.margin.bottom;
 
 		// SVG drawing area
 		vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -44,17 +44,9 @@ class HomeMapVis {
 			.attr('id', 'interTooltip')
 			.attr('class', 'tooltip')
 
-		// lat/lon bounds of continental US
-		let long = [-130, -60]
-		let lat = [26, 51]
-
-		// Add X axis
-		vis.x = d3.scaleLinear().domain(long).range([vis.margin.left, vis.width]);
-		vis.x.clamp(true); // Keep Hawaii and Alaska close
-
-		// Add Y axis
-		vis.y = d3.scaleLinear().domain(lat).range([vis.height, vis.margin.top]);
-		vis.y.clamp(true); // Keep Hawaii and Alaska close
+		vis.projection = d3.geoAlbersUsa()
+			.translate([vis.width / 2, vis.height / 2])
+			.scale(600);
 
 		vis.dataType = "yard"
 
@@ -78,6 +70,9 @@ class HomeMapVis {
 
 		vis.contours = vis.svg.append("g")
 
+		vis.pp = d3.geoPath()
+			.projection(vis.projection);
+
 		vis.states = vis.svg.append("g")
 		vis.states
 			.selectAll(".map-point")
@@ -85,8 +80,9 @@ class HomeMapVis {
 			.enter()
 			.append("circle")
 			.attr("class", "map-point")
-			.attr("cx", d => vis.x(d.longitude))
-			.attr("cy", d => vis.y(d.latitude))
+			.attr("cx", d => vis.projection([d.longitude, d.latitude])[0])
+			.attr("cy", d => vis.projection([d.longitude, d.latitude])[1])
+			// .attr("transform", d => `translate(${vis.projection([d.longitude,d.latitude])})`)
 			.attr("r", 3)
 			.on('mouseover', function(event, d) {
 				d3.select(this)
@@ -113,6 +109,25 @@ class HomeMapVis {
 
 			})
 
+		// SVG drawing area
+		vis.svgLots = d3.select("#" + vis.parentElement).append("svg")
+			.attr("width", vis.width + vis.margin.left + vis.margin.right)
+			.attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+		vis.loty = d3.scaleLinear()
+			.domain([0, 4])
+			.range([vis.margin.top, vis.height]);
+
+		vis.lotx = d3.scaleLinear()
+			.domain([0, 9])
+			.range([vis.margin.left, vis.width]);
+
+		vis.lot2width = d3.scaleSqrt()
+			.domain([0, d3.max(vis.displayData.map(d => d[vis.dataType]))])
+			.range([0, vis.width/30])
+
 		vis.wrangleData();
 	}
 
@@ -130,25 +145,14 @@ class HomeMapVis {
 	updateVis() {
 		let vis = this;
 
+		vis.displayData.sort((a,b) => a[vis.dataType] - b[vis.dataType])
+
 		// compute the density data
 		const densityData = d3
 			.contourDensity()
-			.x(function (d) {
-				return vis.x(d.longitude);
-			}) // x and y = column name in .csv input data
-			.y(function (d) {
-				return vis.y(d.latitude);
-			})
-			.weight(d => {
-				switch (vis.dataType) {
-					case "yard":
-						return d.yard;
-					case "home":
-						return d.home;
-					case "lot":
-						return d.lot;
-				}
-			})
+			.x(d => vis.projection([d.longitude, d.latitude])[0]) // x and y = column name in .csv input data
+			.y(d => vis.projection([d.longitude, d.latitude])[1])
+			.weight(d => d[vis.dataType])
 			.size([vis.width, vis.height])
 			.bandwidth(15)(
 				// smaller = more precision in lines = more lines
@@ -156,16 +160,19 @@ class HomeMapVis {
 			);
 
 		const thresholds = densityData.map((r) => +r.value);
-		let med = d3.median(thresholds)
-		let mean = d3.mean(thresholds)
-		let dev = d3.deviation(thresholds)
-		let extents = [mean - dev, med, mean + dev]
+		let tmed = d3.median(thresholds)
+		let tmean = d3.mean(thresholds)
+		let tdev = d3.deviation(thresholds)
+		let tmin = d3.min(thresholds)
+		let tmax = d3.max(thresholds)
+		// let extents = [tmean - tdev, tmed, tmean + tdev]
+		let extents = [tmin, tmean, tmax]
 		extents.sort((a, b) => a - b);
 
 		const color = d3
 			.scaleLinear()
 			.domain(extents)
-			.range(["#cfe0fd", "white", "#DCAAE3"])
+			.range(["#cfe0fd", "white", "#DFAAFBFF"])
 
 		// Add the contour: several "path"
 		vis.paths = vis.contours.selectAll(".cont").data(densityData)
@@ -174,8 +181,8 @@ class HomeMapVis {
 			.enter()
 			.append("path")
 			.attr("class", "cont")
-			.attr("stroke", "#e3e3e3")
-			.attr("stroke-opacity", 0.15)
+			.attr("stroke", "#ffffff")
+			.attr("stroke-opacity", 0.25)
 			.attr("d", d3.geoPath())
 			.attr("fill", (d) => color(d.value))
 
@@ -186,6 +193,69 @@ class HomeMapVis {
 
 		vis.paths.exit()
 			.remove()
+
+		vis.lots = vis.svgLots.selectAll(".lot")
+			.data(vis.displayData)
+
+		vis.labels = vis.svgLots.selectAll(".state-label")
+			.data(vis.displayData)
+
+		vis.labels
+			.enter()
+			.append("text")
+			.attr("class", "state-label")
+			.text(d => d.state)
+			.attr("x", (d,i) => vis.lotx(i % 10))
+			.attr("y", (d,i) => vis.loty(Math.floor(i/10)) - 10)
+
+		vis.lots
+			.enter()
+			.append("rect")
+			.attr("class", "lot")
+			.attr("x", (d,i) => vis.lotx(i % 10) - vis.lot2width(d.lot)/2)
+			.attr("y", (d,i) => vis.loty(Math.floor(i/10)))
+			.attr("width", d => vis.lot2width(d[vis.dataType]))
+			.attr("height", d => vis.lot2width(d[vis.dataType]))
+			.on('mouseover', function(event, d) {
+				d3.select(this)
+				vis.tooltip
+					.style("opacity", 1)
+					.style("left", event.pageX + 20 + "px")
+					.style("top", event.pageY + "px")
+					.html(`
+                         <div>
+                         	<div></div>
+                             <p><span class="tooltip-emphasis">State:</span> ${d.state}</p>
+                             <p><span class="tooltip-emphasis">Yard:</span> ${(d.yard).toLocaleString()} sqft</p>
+                             <p><span class="tooltip-emphasis">Home:</span> ${(d.home).toLocaleString()} sqft</p>
+                             <p><span class="tooltip-emphasis">Lot:</span> ${(d.lot).toLocaleString()} sqft</p>
+                         </div>`);
+			})
+			.on('mouseout', function (event, d) {
+				d3.select(this)
+				vis.tooltip
+					.style("opacity", 0)
+					.style("left", 0)
+					.style("top", 0)
+					.html(``);
+
+			})
+
+		let max = d3.max(vis.displayData.map(d => d[vis.dataType]))
+		vis.lot2width.domain([0, max])
+
+		vis.lots
+			.merge(vis.lots)
+			.transition()
+			.duration(1000)
+			.attr("x", (d,i) => vis.lotx(i % 10) - vis.lot2width(d[vis.dataType])/2)
+			.attr("y", (d,i) => vis.loty(Math.floor(i/10)))
+			.attr("width", d => vis.lot2width(d[vis.dataType]))
+			.attr("height", d => vis.lot2width(d[vis.dataType]))
+
+		vis.labels
+			.merge(vis.labels)
+			.text(d => d.state)
 
 	}
 }
